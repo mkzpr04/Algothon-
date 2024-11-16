21279ea6ba4e07ca67ef0ab2dee93715e867e320
Mark-Killian <149883245+mkzpr04@users.noreply.github.com>
2024-11-16 12:04:58 +0100

    Add files via upload

---
+++ Strat.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import cryptpandas as crp
+
+df = pd.DataFrame({'A': [1, 2, 3],
+                   'B': ['one', 'one', 'four']})
+
+crp.to_encrypted(df, password='mypassword123', path='file.crypt')
+
+decrypted_df = crp.read_encrypted(path='file.crypt', password='mypassword123')
+
+print((df == decrypted_df).all().all())
